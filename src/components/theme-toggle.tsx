7b71e015e8e0b844cc48c09fd7f3962173ced914
Diljith AK <akdiljith7@gmail.com>
2025-11-01 07:27:28 +0000

    No.. I mean a single icon it will change according to the theme

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -15,8 +15,11 @@ export function ThemeToggle() {
       size="icon"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      {theme === 'dark' ? (
+        <Sun className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem]" />
+      )}
       <span className="sr-only">Toggle theme</span>
     </Button>
   )
