819523df5c00a3e8ce8cd4b694cdaf7a2828b619
Diljith AK <akdiljith7@gmail.com>
2026-02-15 05:43:04 +0000

    Try different colours

--- src/components/ui/background.tsx
+++ src/components/ui/background.tsx
@@ -1,8 +1,8 @@
 export function Background() {
   return (
     <>
-        <div className="fixed inset-0 -z-10 h-full w-full bg-[#f7f7f7] bg-[radial-gradient(circle_at_15%_25%,_hsl(188_82%_58%_/_0.6),_transparent_30%),radial-gradient(circle_at_85%_15%,_hsl(340_82%_58%_/_0.5),_transparent_25%),radial-gradient(circle_at_75%_85%,_hsl(188_82%_58%_/_0.4),_transparent_40%),radial-gradient(circle_at_25%_95%,_hsl(340_82%_58%_/_0.5),_transparent_40%),radial-gradient(circle_at_50%_50%,_hsl(270_80%_60%_/_0.3),_transparent_30%)] dark:hidden" />
-        <div className="fixed inset-0 -z-10 h-full w-full hidden bg-[#020205] bg-[radial-gradient(circle_at_15%_25%,_hsl(188_82%_58%_/_0.6),_transparent_30%),radial-gradient(circle_at_85%_15%,_hsl(340_82%_58%_/_0.5),_transparent_25%),radial-gradient(circle_at_75%_85%,_hsl(188_82%_58%_/_0.4),_transparent_40%),radial-gradient(circle_at_25%_95%,_hsl(340_82%_58%_/_0.5),_transparent_40%),radial-gradient(circle_at_50%_50%,_hsl(270_80%_60%_/_0.3),_transparent_30%)] dark:block" />
+        <div className="fixed inset-0 -z-10 h-full w-full bg-[#f7f7f7] bg-[radial-gradient(circle_at_15%_25%,_hsl(260_80%_60%_/_0.5),_transparent_30%),radial-gradient(circle_at_85%_15%,_hsl(20_90%_60%_/_0.5),_transparent_25%),radial-gradient(circle_at_75%_85%,_hsl(220_90%_55%_/_0.4),_transparent_40%),radial-gradient(circle_at_25%_95%,_hsl(20_90%_60%_/_0.4),_transparent_40%),radial-gradient(circle_at_50%_50%,_hsl(260_80%_60%_/_0.3),_transparent_30%)] dark:hidden" />
+        <div className="fixed inset-0 -z-10 h-full w-full hidden bg-[#020205] bg-[radial-gradient(circle_at_15%_25%,_hsl(260_80%_60%_/_0.5),_transparent_30%),radial-gradient(circle_at_85%_15%,_hsl(20_90%_60%_/_0.5),_transparent_25%),radial-gradient(circle_at_75%_85%,_hsl(220_90%_55%_/_0.4),_transparent_40%),radial-gradient(circle_at_25%_95%,_hsl(20_90%_60%_/_0.4),_transparent_40%),radial-gradient(circle_at_50%_50%,_hsl(260_80%_60%_/_0.3),_transparent_30%)] dark:block" />
     </>
   );
 }
